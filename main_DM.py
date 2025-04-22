1dc8a2d593c2c5ad422c6d53be537883b7778b82
IsitaRex <ec24819@qmul.ac.uk>
2025-04-22 19:17:20 +0100

    Update DM

--- main_DM.py
+++ main_DM.py
@@ -6,8 +6,9 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torchvision.utils import save_image
+import wandb
 from utils import get_loops, get_dataset, get_network, get_eval_pool, evaluate_synset, get_daparam, match_loss, get_time, TensorDataset, epoch, DiffAugment, ParamDiffAug
-
+USE_WANDB = True
 
 def main():
 
@@ -16,15 +17,15 @@ def main():
     parser.add_argument('--model', type=str, default='ConvNet', help='model')
     parser.add_argument('--ipc', type=int, default=50, help='image(s) per class')
     parser.add_argument('--eval_mode', type=str, default='SS', help='eval_mode') # S: the same to training model, M: multi architectures,  W: net width, D: net depth, A: activation function, P: pooling layer, N: normalization layer,
-    parser.add_argument('--num_exp', type=int, default=5, help='the number of experiments')
-    parser.add_argument('--num_eval', type=int, default=20, help='the number of evaluating randomly initialized models')
+    parser.add_argument('--num_exp', type=int, default=1, help='the number of experiments')
+    parser.add_argument('--num_eval', type=int, default=10, help='the number of evaluating randomly initialized models')
     parser.add_argument('--epoch_eval_train', type=int, default=1000, help='epochs to train a model with synthetic data') # it can be small for speeding up with little performance drop
-    parser.add_argument('--Iteration', type=int, default=20000, help='training iterations')
+    parser.add_argument('--Iteration', type=int, default=2000, help='training iterations')
     parser.add_argument('--lr_img', type=float, default=1.0, help='learning rate for updating synthetic images')
     parser.add_argument('--lr_net', type=float, default=0.01, help='learning rate for updating network parameters')
     parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
     parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
-    parser.add_argument('--init', type=str, default='real', help='noise/real: initialize synthetic images from random noise or randomly sampled real images.')
+    parser.add_argument('--init', type=str, default='noise', help='noise/real: initialize synthetic images from random noise or randomly sampled real images.')
     parser.add_argument('--dsa_strategy', type=str, default='color_crop_cutout_flip_scale_rotate', help='differentiable Siamese augmentation strategy')
     parser.add_argument('--data_path', type=str, default='data', help='dataset path')
     parser.add_argument('--save_path', type=str, default='result', help='path to save results')
@@ -33,9 +34,11 @@ def main():
     args = parser.parse_args()
     args.method = 'DM'
     args.outer_loop, args.inner_loop = get_loops(args.ipc)
-    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args.device = 'mps'
     args.dsa_param = ParamDiffAug()
-    args.dsa = False if args.dsa_strategy in ['none', 'None'] else True
+    # args.dsa = False if args.dsa_strategy in ['none', 'None'] else True
+    args.dsa = False
 
     if not os.path.exists(args.data_path):
         os.mkdir(args.data_path)
@@ -43,11 +46,18 @@ def main():
     if not os.path.exists(args.save_path):
         os.mkdir(args.save_path)
 
-    eval_it_pool = np.arange(0, args.Iteration+1, 2000).tolist() if args.eval_mode == 'S' or args.eval_mode == 'SS' else [args.Iteration] # The list of iterations when we evaluate models and record results.
-    print('eval_it_pool: ', eval_it_pool)
+    if USE_WANDB:
+        wandb.init(project=f"Replication-Dataset-Condensation-{args.dataset}", config={
+            "ipc": args.ipc,
+            "batch_size": args.batch_real,
+            "batch_size_train": args.batch_train,
+            "lr_img": args.lr_img,
+            "lr_net": args.lr_net,
+            "model": args.model
+    })
+
+    eval_it_pool = np.arange(0, args.Iteration+1, 500).tolist() if args.eval_mode == 'S' or args.eval_mode == 'SS' else [args.Iteration] # The list of iterations when we evaluate models and record results.
     channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset(args.dataset, args.data_path)
-    print('Hello world!')
-    breakpoint()
     model_eval_pool = get_eval_pool(args.eval_mode, args.model, args.model)
 
 
@@ -67,7 +77,7 @@ def main():
         images_all = []
         labels_all = []
         indices_class = [[] for c in range(num_classes)]
-        breakpoint()
+        
         images_all = [torch.unsqueeze(dst_train[i][0], dim=0) for i in range(len(dst_train))]
         labels_all = [dst_train[i][1] for i in range(len(dst_train))]
         for i, lab in enumerate(labels_all):
@@ -95,7 +105,7 @@ def main():
         if args.init == 'real':
             print('initialize synthetic data from random real images')
             for c in range(num_classes):
-                image_syn.data[c*args.ipc:(c+1)*args.ipc] = get_images(c, args.ipc).detach().data
+                 image_syn.data[c*args.ipc:(c+1)*args.ipc] = get_images(c, args.ipc).detach().data
         else:
             print('initialize synthetic data from random noise')
 
@@ -116,13 +126,20 @@ def main():
                     print('DSA augmentation parameters: \n', args.dsa_param.__dict__)
 
                     accs = []
+                    accs_train = []
                     for it_eval in range(args.num_eval):
                         net_eval = get_network(model_eval, channel, num_classes, im_size).to(args.device) # get a random model
                         image_syn_eval, label_syn_eval = copy.deepcopy(image_syn.detach()), copy.deepcopy(label_syn.detach()) # avoid any unaware modification
                         _, acc_train, acc_test = evaluate_synset(it_eval, net_eval, image_syn_eval, label_syn_eval, testloader, args)
                         accs.append(acc_test)
+                        accs_train.append(acc_train)
                     print('Evaluate %d random %s, mean = %.4f std = %.4f\n-------------------------'%(len(accs), model_eval, np.mean(accs), np.std(accs)))
-
+                    if USE_WANDB:
+                        wandb.log({f"exp_{exp}": {
+                            'iteration': it,
+                            'acc_train': np.mean(accs_train),
+                            'acc_test': np.mean(accs)
+                        }})
                     if it == args.Iteration: # record the final results
                         accs_all_exps[model_eval] += accs
 
@@ -153,16 +170,16 @@ def main():
                 for c in range(num_classes):
                     img_real = get_images(c, args.batch_real)
                     img_syn = image_syn[c*args.ipc:(c+1)*args.ipc].reshape((args.ipc, channel, im_size[0], im_size[1]))
-
                     if args.dsa:
                         seed = int(time.time() * 1000) % 100000
                         img_real = DiffAugment(img_real, args.dsa_strategy, seed=seed, param=args.dsa_param)
                         img_syn = DiffAugment(img_syn, args.dsa_strategy, seed=seed, param=args.dsa_param)
-
+                
                     output_real = embed(img_real).detach()
                     output_syn = embed(img_syn)
 
                     loss += torch.sum((torch.mean(output_real, dim=0) - torch.mean(output_syn, dim=0))**2)
+
 
             else: # for ConvNetBN
                 images_real_all = []
@@ -182,13 +199,11 @@ def main():
 
                 images_real_all = torch.cat(images_real_all, dim=0)
                 images_syn_all = torch.cat(images_syn_all, dim=0)
-
+                
                 output_real = embed(images_real_all).detach()
                 output_syn = embed(images_syn_all)
 
                 loss += torch.sum((torch.mean(output_real.reshape(num_classes, args.batch_real, -1), dim=1) - torch.mean(output_syn.reshape(num_classes, args.ipc, -1), dim=1))**2)
-
-
 
             optimizer_img.zero_grad()
             loss.backward()
@@ -197,7 +212,11 @@ def main():
 
 
             loss_avg /= (num_classes)
-
+            
+            if USE_WANDB:
+                wandb.log({f"exp_{exp}": {
+                    'Synthetic data loss': loss_avg
+                }})
             if it%10 == 0:
                 print('%s iter = %05d, loss = %.4f' % (get_time(), it, loss_avg))
 
